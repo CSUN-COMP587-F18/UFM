4d40be24a510b7ad146b24e7f48aa45e86db66ca
areeba-waheed <areeba.waheed.97@gmail.com>
2018-10-02 23:02:06 -0700

    Addded Java File for test attempt #1

---
+++ app/src/test/java/ufm/universalfinancemanager/UnitTestDemo.java
@@ -0,0 +1,21 @@
+package ufm.universalfinancemanager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Areeba on 10/2/2018.
+ */
+
+public class UnitTestDemo {
+    @Test
+    public void testOne() throws Exception{
+        assertEquals(22, 2*11);
+    }
+
+    @Test
+    public void testTwo() throws Exception{
+        assertEquals(22, 2+11);
+    }
+}
