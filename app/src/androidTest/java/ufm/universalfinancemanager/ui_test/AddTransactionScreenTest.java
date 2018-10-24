39b0e1df1675b6b657996236835d749b20989253
areeba-waheed <areeba.waheed.97@gmail.com>
2018-10-23 21:33:31 -0700

    Adds and deletes a manually input transaction

--- app/src/androidTest/java/ufm/universalfinancemanager/ui_test/AddTransactionScreenTest.java
+++ app/src/androidTest/java/ufm/universalfinancemanager/ui_test/AddTransactionScreenTest.java
@@ -2,6 +2,7 @@ package ufm.universalfinancemanager.ui_test;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -12,9 +13,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import ufm.universalfinancemanager.R;
 import ufm.universalfinancemanager.di.ActivityBindingModule_NetworthActivity;
 import ufm.universalfinancemanager.networth.NetworthActivity;
+import ufm.universalfinancemanager.support.Flow;
 import ufm.universalfinancemanager.transactionhistory.TransactionHistoryActivity;
 import ufm.universalfinancemanager.ufmApplication;
 import ufm.universalfinancemanager.util.EspressoIdlingResource;
@@ -23,9 +28,12 @@ import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Created by Areeba on 10/17/2018.
@@ -66,4 +74,72 @@ public class AddTransactionScreenTest {
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         onView(withText("Add Transaction")).perform(click());
     }
+
+    @Test
+    public void testAddTransaction_isAdded() {
+        addTestTransaction(1, 10.00, Flow.OUTCOME,
+                "food", null, "account1", null);
+
+        onView(withText("TRANSACTION 1")).check(matches(isDisplayed()));
+        //deleteTransaction("TRANSACTION 1");
+    }
+
+    public void addTestTransaction(int number, double amount, Flow flow, String category,
+                                   String toAccount, String fromAccount,
+                                   String notes) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy", Locale.US);
+
+        openAddTransactionMenu();
+
+        onView(withId(R.id.name)).perform(typeText("TRANSACTION " + number),
+                ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.amount)).perform(typeText(Double.toString(amount)),
+                ViewActions.closeSoftKeyboard());
+
+        if (flow == Flow.INCOME) {
+            onView(withId(R.id.flow_income)).perform(click());
+
+            onView(withId(R.id.toaccount)).perform(click());
+            onView(withText(toAccount)).perform(click());
+        } else if (flow == Flow.OUTCOME) {
+            onView(withId(R.id.flow_expense)).perform(click());
+
+            onView(withId(R.id.fromaccount)).perform(click());
+            onView(withText(fromAccount)).perform(click());
+        } else if(flow == Flow.TRANSFER) {
+            onView(withId(R.id.flow_transfer)).perform(click());
+
+            onView(withId(R.id.toaccount)).perform(click());
+            onView(withText(toAccount)).perform(click());
+            onView(withId(R.id.fromaccount)).perform(click());
+            onView(withText(fromAccount)).perform(click());
+        }
+
+        //select category from spinner
+        onView(withId(R.id.category)).perform(click());
+        onView(withText(category)).perform(click());
+
+        if(notes != null)
+            onView(withId(R.id.notes)).perform(typeText(notes),
+                    ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.done)).perform(click());
+    }
+
+    @Test
+    public void testDeleteTransaction_isGone() {
+        addTestTransaction(1, 1.00, Flow.OUTCOME,
+                "food", null, "account1", null);
+
+        deleteTransaction("TRANSACTION 1");
+        onView(withText("TRANSACTION 1")).check(matches(not(isDisplayed())));
+    }
+
+    public void deleteTransaction(String name) {
+        onView(withText(name)).perform(click());
+        onView(withId(R.id.name)).perform(ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.cancel)).perform(click());
+    }
+
+
 }
